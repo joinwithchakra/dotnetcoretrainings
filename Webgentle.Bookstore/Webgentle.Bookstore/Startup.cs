9c57811ec2e929a4e289dae428fbdb45e95289b1
joinwithchakra <76445582+joinwithchakra@users.noreply.github.com>
2021-07-29 00:33:19 +0530

    startup method configureservices

--- Webgentle.Bookstore/Webgentle.Bookstore/Startup.cs
+++ Webgentle.Bookstore/Webgentle.Bookstore/Startup.cs
@@ -16,6 +16,7 @@ namespace Webgentle.Bookstore
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            //this is an optional method.
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
