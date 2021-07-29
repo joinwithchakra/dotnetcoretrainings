200823fefd86d45aea4faa5b55986c570d4d82fa
joinwithchakra <76445582+joinwithchakra@users.noreply.github.com>
2021-07-29 18:36:26 +0530

    Added MVC to application.

--- Webgentle.Bookstore/Webgentle.Bookstore/Startup.cs
+++ Webgentle.Bookstore/Webgentle.Bookstore/Startup.cs
@@ -17,24 +17,51 @@ namespace Webgentle.Bookstore
         public void ConfigureServices(IServiceCollection services)
         {
             //this is an optional method.
+            services.AddControllersWithViews();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+            
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
             }
 
+            //app.Use(async(context, next)=>
+            //{
+            //    await context.Response.WriteAsync("ContentRootPath : " + env.ContentRootPath);
+            //    await context.Response.WriteAsync("<br/>");
+            //    await context.Response.WriteAsync("WebRootPath : " + env.WebRootPath);
+            //    await context.Response.WriteAsync("<br/>");
+            //    await context.Response.WriteAsync("WebRootFileProvider : " + env.WebRootFileProvider);
+            //    await context.Response.WriteAsync("<br/>");
+            //    await context.Response.WriteAsync("ContentRootFileProvider : " + env.ContentRootFileProvider);
+            //    await context.Response.WriteAsync("<br/>");
+
+            //    await context.Response.WriteAsync("ApplicationName : " + env.ApplicationName);
+            //    await context.Response.WriteAsync("<br/>");
+            //    await context.Response.WriteAsync("ContentRootFileProvider : " + env.ContentRootFileProvider);
+            //    await context.Response.WriteAsync("<br/>");
+            //    await context.Response.WriteAsync("EnvironmentName : " + env.EnvironmentName);
+            //    await context.Response.WriteAsync("<br/>");
+            //    await context.Response.WriteAsync("ContentRootPath : " + env.WebRootPath);
+            //    await context.Response.WriteAsync("<br/>");
+
+            //    await next();
+            //});
+
             app.UseRouting();
 
             app.UseEndpoints(endpoints =>
             {
-                endpoints.MapGet("/", async context =>
-                {
-                    await context.Response.WriteAsync("Hello World!");
-                });
+                //endpoints.MapGet("/", async context =>
+                //{
+                //    await context.Response.WriteAsync("Hello World!");
+                //});
+
+                endpoints.MapDefaultControllerRoute();
             });
         }
     }
